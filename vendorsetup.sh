832fb0105e14e52345b7a682bd0dd91e30b18988
eidawon <projecteidawon@gmail.com>
2025-05-30 10:20:17 +0800

    sweet: Axion-ify!
    Change-Id: Ie815cef8d19246786e8744faf55944f6f014c880
    Signed-off-by: eidawon <projecteidawon@gmail.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+# Inherit Miui/Leica Camera
+echo "Cloning MiuiCamera..."
+git clone https://github.com/Eida-at-22-00/vendor_xiaomi_miuicamera-sweet vendor/xiaomi/miuicamera-sweet/
+
+echo "Pulling LFS files..."
+cd vendor/xiaomi/miuicamera-sweet/ && git lfs pull && cd ../../..
+
+# Signed-keys
+echo "Cloning Signed-keys..."
+git clone https://github.com/Evolution-X-djl/vendor_lineage-priv_keys vendor/lineage-priv/keys/
+
+echo "All operations completed successfully!"
